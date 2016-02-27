121f391e3bcd08d9d94893456304dea9cee714bf
Kirill Fomichev <fanatid@ya.ru>
2016-02-27 11:21:08 +0300

    Add main function

--- README.md
+++ README.md
@@ -6,4 +6,31 @@
 
 [![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
 
+Based on ideas from [challenges-optimizing-unspent-output-selection (BitGo)](https://blog.bitgo.com/challenges-optimizing-unspent-output-selection/)
+
 ## API
+
+##### `function utxoSelection (utxos, target, options)`
+
+- `utxos`: *Array* of *Objects*:
+  - `address`: *String*
+  - `value`: *Number*
+- `target`: *Number* (in satoshis)
+- `options`: *Object*
+  - `minChange`: *?Number* (by default 546 satoshi)
+  - `fee`: *?Number* (fee as constant)
+  - `feePerKB`: *?Number* (by default 20000 satoshis)
+  - `extraBytes`: *?Number* (count of extra bytes for fee calculation, by default: 0)
+  - `outputs`: *?Number* (number of outputs, by default 1)
+
+Fee calculated on next formula:
+```
+(10 + inputs * 148 + outputs * 34 + extra_bytes) * feePerKB / 1000
+```
+where:
+
+```
+10 = 4 (version) + 4 (locktime) + 1 (inputs count) + 1 (outputs count)
+148 = 32 (txId) + 4 (vout) + 1 (P2PKH scriptSig length) + 107 (P2PKH scriptSig) + 4 (sequence)
+34 = 8 (value) + 4 (P2PKH scriptPubKey length) + 25 (P2PKH scriptPubKey)
+```
---
+++ src/index.js
@@ -0,0 +1,62 @@
+import { get, groupBy, sortBy, sumBy } from 'lodash'
+
+export let getFeeEstimator = function (options) {
+  // in most cases (P2PKH):
+  // 10 = version: 4, locktime: 4, inputs and outputs count: 1
+  // 148 = txId: 32, vout: 4, count: 1, script: 107, sequence: 4
+  // 34 = value: 8, count: 1, scriptPubKey: 25
+  if (typeof get(options, 'fee') === 'number') return () => get(options, 'fee')
+  let feePerByte = get(options, 'feePerKB', 20000) / 1000
+  let extraBytes = get(options, 'extraBytes', 0)
+  let outputsFee = get(options, 'outputs', 1) * 34
+  return (utxos) => Math.ceil((10 + utxos.length * 148 + outputsFee + extraBytes) * feePerByte)
+}
+
+export let select = function (utxos, feeEstimator, target) {
+  // simple, but not optimized selection
+  let result = { utxos: [], value: 0 }
+  for (let utxo of sortBy(utxos, 'value')) {
+    result.push(utxo)
+    result.value += utxo.value
+    if (result.value - feeEstimator(result.utxo) >= target) return result
+  }
+
+  return null
+}
+
+export default function utxoSelection (utxos, target, options) {
+  let minChange = get(options, 'minChange', 546)
+  let feeEstimator = getFeeEstimator(options)
+
+  // check that total sum is enough
+  let total = utxos.reduce((t, utxo) => t + utxo.value, 0) - feeEstimator(utxos)
+  if (total < target) return null
+  if (total < target + minChange) return utxos
+
+  // select only one utxo (tx without change)
+  let solution1 = utxos.reduce((solution, utxo) => {
+    let value = utxo.value - feeEstimator([utxo])
+    if (value >= target && value < target + minChange && value < solution[0].value) return [utxo]
+    return solution
+  }, [{ value: Infinity }])
+  if (isFinite(solution1[0].value)) return solution1
+
+  // select group of utxos related with one address
+  let grouped = groupBy(utxos, 'address')
+  let solution2 = Object.keys(grouped).reduce((solution, address) => {
+    let selected = select(grouped[address], feeEstimator, target)
+    if (selected === null) return solution
+
+    let {utxos, value} = selected
+    let fee = feeEstimator(utxos)
+    if (value < target + fee) return solution
+
+    let solutionValue = sumBy(solution, 'value')
+    if (value < target + fee + minChange) return value < solutionValue ? utxos : solution // tx without change?
+    return (isFinite(solution[0].value) && solution.length < utxos.length) ? solution : utxos
+  }, [{ value: Infinity }])
+  if (isFinite(solution2[0].value)) return solution2
+
+  // select group
+  return select(utxos, feeEstimator, target).utxos
+}
